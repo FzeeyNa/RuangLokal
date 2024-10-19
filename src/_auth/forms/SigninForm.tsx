67b1e5e98f5e34f4ee603a57575b0b5c092a9909
FzeeyNa <zeerayzan@gmail.com>
2024-10-19 23:27:51 +0700

    first commit

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -54,7 +54,7 @@ const SigninForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/public/assets/images/Group_7.svg" alt="logo" />
+        <img src="/public/assets/images/logo.svg" alt="logo" />
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
         Masuk ke akun Anda
